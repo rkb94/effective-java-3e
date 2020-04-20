c5f1014ad5a5e577bcc535f49a45dd3369cf61d4
Kyungbin Ryu <ryukbin94@gmail.com>
2020-04-21 01:12:08 +0900

    Add examples to LambdaTest

--- src/chapter7/item42/LambdaTest.java
+++ src/chapter7/item42/LambdaTest.java
@@ -1,6 +1,8 @@
 package chapter7.item42;
 
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
@@ -8,13 +10,29 @@ public class LambdaTest {
     public static void main(String[] args) {
         List<String> words = Arrays.asList("55555", "333", "1", "4444", "22");
 
-        words.sort(new Comparator<String>() {
+        // Anonymous Class
+        Collections.sort(words, new Comparator<String>() {
             @Override
-            public int compare(String o1, String o2) {
-                return Integer.compare(o1.length(), o2.length());
+            public int compare(String s1, String s2) {
+                return Integer.compare(s1.length(), s2.length());
             }
         });
+        System.out.println("Anonymous class : " + words);
+        Collections.shuffle(words);
 
-        System.out.println(words);
+        // Lambda
+        Collections.sort(words, (s1, s2) -> Integer.compare(s1.length(), s2.length()));
+        System.out.println("Using Lambda : " + words);
+        Collections.shuffle(words);
+
+        // Method reference
+        Collections.sort(words, Comparator.comparingInt(String::length));
+        System.out.println("Using Method reference : " + words);
+        Collections.shuffle(words);
+
+        // Method reference
+        words.sort(Comparator.comparingInt(String::length));
+        System.out.println("Using Method reference in sort of List : " + words);
+        Collections.shuffle(words);
     }
 }
